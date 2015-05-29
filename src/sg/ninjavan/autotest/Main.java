2b430968fea214e7fb0cfc42794ba59dee68ffcc
ngzhongqin <ngzhongqin@gmail.com>
2015-05-30 02:02:03 +0800

    Log4j

--- src/sg/ninjavan/autotest/Main.java
+++ src/sg/ninjavan/autotest/Main.java
@@ -6,9 +6,8 @@ import org.apache.log4j.xml.DOMConfigurator;
 public class Main {
     private static Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello");
         DOMConfigurator.configure("log4j.xml");
-
+        PropertyConfigurator.configure("log4j.properties");
+        logger.info("start");
     }
 }
